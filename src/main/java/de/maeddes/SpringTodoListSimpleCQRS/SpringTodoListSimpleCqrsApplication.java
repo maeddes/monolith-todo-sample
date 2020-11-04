f42f8a4300345786e3eb2633433cc7f834de8575
Matthias Haeussler <maeddes@gmail.com>
2020-11-04 16:08:05 +0100

    base setting (v1)

--- src/main/java/de/maeddes/SpringTodoListSimpleCQRS/SpringTodoListSimpleCqrsApplication.java
+++ src/main/java/de/maeddes/SpringTodoListSimpleCQRS/SpringTodoListSimpleCqrsApplication.java
@@ -2,23 +2,11 @@ package de.maeddes.SpringTodoListSimpleCQRS;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -49,7 +37,7 @@ public class SpringTodoListSimpleCqrsApplication {
 	String hello() {
 
 		logger.info(" Call to hello method on instance: " + getInstanceId());
-		return getInstanceId() + " Hello, cf-for-k8s ! (v2) ";
+		return getInstanceId() + " Hello, cf-for-k8s ! ";
 
 	}
 
@@ -57,7 +45,7 @@ public class SpringTodoListSimpleCqrsApplication {
 	String fail() {
 
 		logger.info("Call to failing method on instance: " + getInstanceId());
-		//System.exit(1);
+		System.exit(1);
 		return "fixed!";
 	}
 
